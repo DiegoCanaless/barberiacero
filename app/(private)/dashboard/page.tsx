6fb650185bd62c50b290068120dcc05cd27e9b2c
Diego Canales <diegocanales112@gmail.com>
2026-03-31 13:15:22 -0300

    Arreglo de error al deployar

--- app/(private)/dashboard/page.tsx
+++ app/(private)/dashboard/page.tsx
@@ -3,24 +3,43 @@ import { jwtVerify } from "jose";
 import SuperAdminPanel from "@/components/dashboard/SuperAdminPanel";
 import UsuarioPanel from "@/components/dashboard/UsuarioPanel";
 import BarberoPanel from "@/components/dashboard/BarberoPanel";
+import { UserResponseDTO } from "@/types/entities/user/UserResponseDTO";
+import { State } from "@/types/enum/estado";
 
 export default async function DashboardPage() {
-    
+
     const token = (await cookies()).get("token")?.value;
 
-    const { payload } = await jwtVerify(
-        token!,
-        new TextEncoder().encode(process.env.JWT_SECRET!)
-    );
+    type JWTPayloadCustom = {
+    id: string;
+    name: string;
+    apellido: string;
+    email: string;
+    role: "usuario" | "admin" | "barber";
+    telefono: string;
+    estado: State;
+    created_at: string;
+};
 
-    const user = {
-        id: payload.id as string,
-        name: payload.name as string,
-        apellido: payload.apellido as string,
-        email: payload.email as string,
-        role: payload.role as string,
-        telefono: payload.telefono as string
-    };
+
+    const { payload } = await jwtVerify<JWTPayloadCustom>(
+    token!,
+    new TextEncoder().encode(process.env.JWT_SECRET!)
+);
+
+
+    const user: UserResponseDTO = {
+    id_usuario: payload.id,
+    name: payload.name,
+    apellido: payload.apellido,
+    email: payload.email,
+    role: payload.role,
+    telefono: payload.telefono,
+    estado: payload.estado,
+    created_at: payload.created_at
+};
+
+
 
     if (user.role === "admin") return <SuperAdminPanel />;
     if (user.role === "barber") return <BarberoPanel />;
